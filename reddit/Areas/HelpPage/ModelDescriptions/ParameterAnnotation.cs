699717e85f0b821d9765adbd97571ef12a8fb6a9
James Sturtevant <jsturtevant@gmail.com>
2015-09-15 16:00:47 -0400

    initial commit

---
+++ reddit/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace reddit.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ reddit/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace reddit.Controllers
+{
+    public class ThumperController : ApiController
+    {
+        // GET api/values
+        public bool Get(string message)
+        {
+            return true;
+        }
+
+      
+    }
+}
